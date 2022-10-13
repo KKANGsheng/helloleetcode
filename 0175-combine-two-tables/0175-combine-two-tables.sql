2a72af60a9a4e22fd57bd4e252b7abe4003980f1
KKANGsheng <90913435+KKANGsheng@users.noreply.github.com>
2022-10-13 12:13:02 +0800

    Time: 799 ms (67.11%), Space: 0B (100.00%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,5 +1,4 @@
-# Write your MySQL query statement below
-
+/* Write your T-SQL query statement below */
 select Person.firstname,Person.lastname,Address.city,Address.State
 from Person
 left join Address
