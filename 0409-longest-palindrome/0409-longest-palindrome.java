e17b3dd2198bf3060517888efbc650625626790d
KKANGsheng <90913435+KKANGsheng@users.noreply.github.com>
2025-06-27 23:57:26 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        int[]characters=new int[128];
+
+        for(char c:s.toCharArray()){
+            characters[c]++;
+        }
+
+        int result=0;
+        for(int i:characters){
+            result+=i/2*2;
+        }
+
+        if(result<s.length){
+            result+=1;
+        }
+
+        return result;
+
+
+    }
+}
