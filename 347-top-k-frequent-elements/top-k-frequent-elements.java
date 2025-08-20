5ddc955c7c51dd5c433711a932e77981ce190cae
KKANGsheng <90913435+KKANGsheng@users.noreply.github.com>
2025-08-20 23:47:56 +0800

    Time: 14 ms (57.95%) | Memory: 49 MB (34.53%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -7,20 +7,20 @@ class Solution {
         for(int num:nums){
             map.put(num,map.getOrDefault(num,0)+1);
         }
+        
+    PriorityQueue<Map.Entry<Integer, Integer>> heap =
+    new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
 
-        PriorityQueue<Map.Entry<Integer,Integer>> heap=
-        new PriorityQueue<>((a,b)->a.getValue()-b.getValue());
-
-        for(Map.Entry<Integer,Integer>entry:map.entrySet()){
-            heap.add(entry);
-            if(heap.size()>k){
-                heap.poll();
-            }
+    for(Map.Entry<Integer,Integer>entry:map.entrySet()){
+        heap.add(entry);
+        if(heap.size()>k){
+            heap.poll();
         }
+    }
 
-        while(!heap.isEmpty()){
-            result[index++]=heap.poll().getKey();
-        }
+    while(!heap.isEmpty()){
+        result[index++]=heap.poll().getKey();
+    }
 
 
         return result;
