2bebe1de000a808f8121251e16c9966c87c3973c
KKANGsheng <90913435+KKANGsheng@users.noreply.github.com>
2025-09-27 23:07:07 +0800

    Time: 0 ms (100.00%) | Memory: 42.1 MB (9.90%) - LeetSync

--- 953-reverse-only-letters/reverse-only-letters.java
+++ 953-reverse-only-letters/reverse-only-letters.java
@@ -1,8 +1,9 @@
 class Solution {
     public String reverseOnlyLetters(String s) {
-        char[] chars=s.toCharArray();
+        char [] chars=s.toCharArray();
         int left=0;
         int right=s.length()-1;
+
 
         while(left<right){
             while(left<right && !Character.isLetter(chars[left])){
@@ -12,24 +13,13 @@ class Solution {
             while(left<right && !Character.isLetter(chars[right])){
                 right--;
             }
-
             char temp=chars[left];
             chars[left]=chars[right];
             chars[right]=temp;
             left++;
             right--;
         }
-
         return new String(chars);
-
-
-    }
-
-
-    public boolean isLetterOrDigit(char c){
-        if(Character.isLetterOrDigit(c)){
-            return true;
-        }
-        return false;
+ 
     }
 }
