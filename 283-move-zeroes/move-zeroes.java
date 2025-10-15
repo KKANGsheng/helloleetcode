3624dc3f8532f29994e81639ad5d3a08cd8bf1f2
KKANGsheng <90913435+KKANGsheng@users.noreply.github.com>
2025-10-15 23:48:19 +0800

    Time: 123 ms (5.02%) | Memory: 46.2 MB (56.13%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -5,13 +5,15 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=0){
                 nums[j]=nums[i];
+                System.out.println("nums[j] "+nums[j]);
                 j++;
             }
         }
 
-        while(j<nums.length){
-            nums[j]=0;
-            j++;
+        for(int k=j;k<nums.length;k++){
+            nums[k]=0;
         }
+
+
     }
 }
