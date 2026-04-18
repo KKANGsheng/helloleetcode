27988c2ac392eefaf76747edb1b9ea7ad226188f
KKANGsheng <90913435+KKANGsheng@users.noreply.github.com>
2026-04-19 00:36:39 +0800

    Time: 1 ms (99.93%) | Memory: 46.1 MB (91.42%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,21 +1,20 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        int []counts= new int[26];
+    int []result =new int[26];
 
-        for(char a:magazine.toCharArray()){
-            counts[a-'a']++;
-        }
-
-        for(char b:ransomNote.toCharArray()){
-            counts[b-'a']--;
-        }
-
-        for(int count:counts){
-            System.out.println("a" +count);
-            if(count<0){
-                return false;
-            }
-        }
-        return true;
+    for(char a:magazine.toCharArray()){
+        result[a-'a']++;
     }
+
+    for(char b:ransomNote.toCharArray()){
+        result[b-'a']--;
+    }
+
+    for(int i=0;i<result.length;i++){
+        if(result[i]<0){
+            return false;
+        }
+    }
+    return true;
+ }
 }
