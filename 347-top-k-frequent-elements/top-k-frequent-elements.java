d4d5311f524428f00be14f1ad22c5d5098255652
KKANGsheng <90913435+KKANGsheng@users.noreply.github.com>
2026-04-20 19:19:53 +0800

    Time: 14 ms (71.28%) | Memory: 47.6 MB (68.71%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -3,24 +3,21 @@ class Solution {
         HashMap<Integer,Integer>map= new HashMap<>();
         PriorityQueue<Map.Entry<Integer,Integer>>minHeap=
         new PriorityQueue<>((a,b)->a.getValue()-b.getValue());
-        int []result =new int[k];
 
-        // Store All the frequency
         for(int i=0;i<nums.length;i++){
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
 
         for(Map.Entry<Integer,Integer>entry:map.entrySet()){
-            minHeap.add(entry);
+            minHeap.offer(entry);
             if(minHeap.size()>k){
                 minHeap.poll();
             }
         }
-
+        int []result= new int[k];
         for(int i=0;i<k;i++){
             result[i]=minHeap.poll().getKey();
         }
-
         return result;
 
     }
