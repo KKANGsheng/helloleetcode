3fb703bbe26cf17027d08d923e644ff16b45c907
KKANGsheng <90913435+KKANGsheng@users.noreply.github.com>
2026-05-11 11:59:12 +0800

    Time: 33 ms (21.77%) | Memory: 46.6 MB (91.56%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -1,16 +1,16 @@
 class Solution {
     public int firstUniqChar(String s) {
-        HashMap<Character,Integer> hashMap = new HashMap<>();
+    HashMap<Character,Integer> hashMap = new HashMap<>();
 
-        for (char c:s.toCharArray()) {
-            hashMap.put(c,hashMap.getOrDefault(c,0)+1);
-        }
+    for (int i=0;i<s.length();i++) {
+        hashMap.put(s.charAt(i),hashMap.getOrDefault(s.charAt(i),0)+1);
+    }
 
-        for (int i=0;i<s.length();i++) {
-            if(hashMap.get(s.charAt(i))== 1){
-                return i;
-            }
+    for(int j=0;j<s.length();j++) {
+        if(hashMap.get(s.charAt(j))==1){
+            return j;
         }
-        return -1;
+    }
+    return -1;
   }
 }
