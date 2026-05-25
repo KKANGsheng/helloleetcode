fe3d9729a327ed1bd5b5c5766e3291214706be94
KKANGsheng <90913435+KKANGsheng@users.noreply.github.com>
2026-05-25 21:28:39 +0800

    Time: 6 ms (88.79%) | Memory: 43.6 MB (92.46%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,19 +1,19 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        char[] sChars = new char[256];
-        char[] tChars = new char[256];
+    char [] sChars = new char[256];
+    char [] tChars = new char[256];
 
-        for (int i=0;i<s.length();i++) {
-            char sChar = s.charAt(i);
-            char tChar = t.charAt(i);
+    for (int i=0;i<s.length();i++) {
+        char sChar = s.charAt(i);
+        char tChar = t.charAt(i);
 
-            if(sChars[sChar] ==0 && tChars[tChar] ==0) {
-                sChars[sChar] = tChar;
-                tChars[tChar] = sChar;
-            }else if (sChars[sChar]!=tChar) {
-                return false;
-            }
+        if(sChars[s.charAt(i)]==0 && tChars[t.charAt(i)]==0) {
+            sChars[sChar] = tChar;
+            tChars[tChar] = sChar;
+        } else if(sChars[sChar]!=tChar) {
+            return false;
         }
-            return true;
+    }
+        return true;
     }
 }
